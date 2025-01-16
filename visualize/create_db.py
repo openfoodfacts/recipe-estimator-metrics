e50c4cc2d19b0564aa822e9afb50facb417b5839
Alex Garel <alex@garel.org>
2025-01-16 17:47:52 +0100

    feat(wip): populating script to visualize

--- visualize/create_db.py
+++ visualize/create_db.py
@@ -1,6 +1,7 @@
 # this scripts takes the results from a one or more test runs and creates a database to help visualize the results
 
 import argparse
+import collections
 import contextlib
 import glob
 import itertools
@@ -8,6 +9,7 @@ import json
 import os
 
 import duckdb
+import pandas as pd
 
 
 def parse_args(args=None):
@@ -22,11 +24,11 @@ def parse_args(args=None):
 
 
 def test_name(results_path):
-    return results_path.split('/')[-1]
+    return results_path.strip("/").split('/')[-1]
 
 
 def model_name(results_path):
-    return results_path.split('/')[-2]
+    return results_path.strip("/").split('/')[-2]
 
 
 @contextlib.contextmanager
@@ -112,6 +114,33 @@ def num_ciqual(ingredients):
     )
 
 
+def next_seq(sequences, name, key=None):
+    """Try to get the right sequence number for a table row
+
+    if key is not None, we might seek for a previous sequence number for this key
+
+    It then sets current sequence for the table
+    """
+    if name not in sequences:
+        sequences[name] = {"seq": 0, "keys": {}, "current": None}
+    if key:
+        is_new = False
+        if key not in sequences[name]["keys"]:
+            sequences[name]["seq"] += 1
+            sequences[name]["keys"][key] = sequences[name]["seq"]
+            is_new = True
+        sequences[name]["current"] = sequences[name]["keys"][key]
+        return sequences[name]["current"], is_new
+    else:
+        sequences[name]["seq"] += 1
+        sequences[name]["current"] = sequences[name]["seq"]
+        return sequences[name]["current"]
+
+
+def current_seq(sequences, name):
+    return sequences[name]["current"]
+
+
 def populate_ingredients_statement(ingredient, result_id, position, is_sub=False):
     yield (
         """
@@ -138,69 +167,46 @@ def populate_ingredients_statement(ingredient, result_id, position, is_sub=False
             yield from populate_ingredients_statement(sub_ingredient, result_id, is_sub=True)
 
 
-def populate_statements(results_path):
-    yield (
-        "INSERT OR IGNORE INTO models VALUES (nextval('seq_models_id'), ?)",
-        (model_name(results_path),),
-    )
-    yield (
-        "SET VARIABLE model_id = (SELECT id FROM models WHERE name = ?)",
-        (model_name(results_path),),
-    )
-    # create the test
-    yield (
-        "INSERT INTO tests VALUES (nextval('seq_tests_id'), ?)",
-        (test_name(results_path),),
-    )
-    yield (
-        "SET VARIABLE test_id = (SELECT id FROM tests WHERE name = ?)",
-        (test_name(results_path),),
-    )
+def populate_statements(results_path, sequences):
+    model_id, is_new = next_seq(sequences, "models", model_name(results_path))
+    if is_new:
+        yield ("models", {
+            "id": model_id,
+            "name": model_name(results_path),
+        })
+    test_id, is_new = next_seq(sequences, "tests", test_name(results_path))
+    if is_new:
+        yield ("tests", {
+            "id": test_id,
+            "name": test_name(results_path),
+        })
     for json_path in glob.glob(f"{results_path}/*.json"):
         with open(json_path) as f:
             if json_path.endswith("results_summary.json"):
                 continue
             code = json_path.split("/")[-1].split(".")[0]
             result = json.load(f)
-            yield from result_statements(code, result)
+            yield from result_statements(sequences, code, result)
 
 
-def result_statements(code, result):
+def result_statements(sequences, code, result):
     metrics = result["ingredients_metrics"]
-    yield (
-        """
-        INSERT INTO results
-        (id, test_id, model_id, code, num_ingredients, num_ingredients_total, num_ciqual, total_specified_input_percent, total_difference, relative_difference)
-        VALUES (nextval('seq_results_id'), getvariable('test_id'),getvariable('model_id'), ?, ?, ?, ?, ?, ?, ?)
-        """,
-        (
-            code,
-            len(result["ingredients"]),
-            num_ingredients_total(result["ingredients"]),
-            num_ciqual(result["ingredients"]),
-            metrics.get("total_specified_input_percent"),
-            metrics.get("total_difference"),
-            metrics.get("relative_difference"),
-        )
-    )
-    yield (
-        """
-        SET VARIABLE result_id = (
-            SELECT id FROM results WHERE  getvariable('test_id') AND code = ?)
-        """,
-        (code,),
-    )
+    yield ("results", {
+        "id": next_seq(sequences, "results"),
+        "test_id": current_seq(sequences, "tests"),
+        "model_id": current_seq(sequences, "models"),
+        "code": code,
+        "num_ingredients": len(result["ingredients"]),
+        "num_ingredients_total": num_ingredients_total(result["ingredients"]),
+        "num_ciqual": num_ciqual(result["ingredients"]),
+        "total_specified_input_percent": metrics.get("total_specified_input_percent"),
+        "total_difference": metrics.get("total_difference"),
+        "relative_difference": metrics.get("relative_difference"),
+    })
     for ingredient in result["ingredients"]:
         pass  # FIXME
-    for category in result["categories_tags"]:
-        yield (
-            """
-            INSERT INTO categories
-            (id, result_id, name)
-            VALUES (nextval('seq_categories_id'), getvariable('result_id'), ?)
-            """,
-            (category,),
-        )
+    for category in result.get("categories_tags", []):
+        yield ("categories", {"id": next_seq(sequences, "categories"), "result_id": current_seq(sequences, "results"), "name": category})
 
 
 def batched(iter, n):
@@ -212,22 +218,35 @@ def batched(iter, n):
 
 
 def populate_db(db_path, statements):
+    ORDERS = {
+        "models": 1,
+        "tests": 2,
+        "results": 3,
+        "ingredients": 4,
+        "categories": 5,
+    }
     with test_db(db_path) as conn:
-        for statement in statements:
-            conn.execute(*statement)
+        # create dataframes
+        datas = sorted(statements, key=lambda x: (ORDERS[x[0]], x[0], x[1].get("id")))
+        for table, data in itertools.groupby(datas, lambda x: x[0]):
+            data_df = pd.DataFrame([d[1] for d in data])
+            conn.execute(f"INSERT INTO {table} SELECT * FROM data_df")
 
 
-def process_path(db_path, results_path):
+def process_path(db_path, results_path, sequences=None):
     if glob.glob(f"{results_path}/*.json"):
-        populate_db(db_path, populate_statements(results_path))
+        populate_db(db_path, populate_statements(results_path, sequences))
     else:
         # recurse
         for sub_path in glob.glob("results_path/*"):
             if os.path.isdir(sub_path):
-                process_path(db_path, sub_path)
+                process_path(db_path, sub_path, sequences)
 
 if __name__ == "__main__":
     args = parse_args()
     create_db(args.db_path)
+    # help having unique ids per models
+    # if we want to update an existing db, we should get sequences from database
+    sequences = {}
     for results_path in args.results_path:
-        process_path(args.db_path, results_path)
+        process_path(args.db_path, results_path, sequences)
