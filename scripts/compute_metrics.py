8f21be5785274db86522bcebaa02a533e601420d
John Gomersall <thegoms@gmail.com>
2025-01-21 11:18:36 +0000

    First test run
    Signed-off-by: John Gomersall <thegoms@gmail.com>

--- scripts/compute_metrics.py
+++ scripts/compute_metrics.py
@@ -239,7 +239,7 @@ def compute_metrics_for_test_set(results_path, test_set_name):
 
         # Save a summary of the results in the test set directory        
         test_set_average_difference = test_set_total_difference / test_set_number_of_products
-        all_ciqual_test_set_average_difference = all_ciqual_test_set_total_difference / all_ciqual_test_set_number_of_products
+        all_ciqual_test_set_average_difference = -1 if all_ciqual_test_set_number_of_products == 0 else all_ciqual_test_set_total_difference / all_ciqual_test_set_number_of_products
 
         results_summary = {
             "test_set_name": test_set_name,
